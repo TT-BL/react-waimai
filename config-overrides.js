41942cf061c77ac4d1906c589e82d36c7d141796
TT-BL <2207265315@qq.com>
2020-04-07 18:00:04 +0800

    初始化项目

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const { override, addLessLoader,fixBabelImports,addDecoratorsLegacy } = require('customize-cra') 
+const theme=require('./theme')
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    // addDecoratorsLegacy(),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyvars:theme,
+
+    }),
+)
